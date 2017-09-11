cf526a0e5be28421d3c7c28a368e42fa05884523
afnanHaddab <afnan.alhaddab@gmail.com>
2017-09-11 05:16:37 +0300

    First commit

---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'author': 'Afnan',
+            'create_date':'04-09-2017'
+
+        },
+        {
+            'id':2,
+            'title':'Article Two',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'author': 'Sara',
+            'create_date':'04-09-2017'
+
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'author': 'Nouf',
+            'create_date':'04-09-2017'
+
+        }
+    ]
+    return articles
